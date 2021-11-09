e64f2a89096ccba2117184cc809e75d6d2f85ea6
Ethan <e.cowey1@newcastle.ac.uk>
2021-11-09 22:57:04 +0000

    Ground works for login t the dashboard page

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Dashboard from './components/dashboard/dashboard';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button> click here</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1> HI </h1>
+      <BrowserRouter>
+          <Routes>
+          <Route path="/dashboard" element={<Dashboard />}>
+          </Route>
+          </Routes>
+      </BrowserRouter>
     </div>
   );
 }
