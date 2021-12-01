943d68be9d49e117057a6775f7efdaf8b5f9e664
Ethan <e.cowey1@newcastle.ac.uk>
2021-12-01 17:45:02 +0000

    Cinnected uniqueUsername to index.js

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser')
 const bcrypt = require('bcryptjs')
 
 const register = require('./components/register')
+const uniqueUsername = require('./components/uniqueUsername')
 const login = require('./components/login')
 const post = require('./components/addPost')
 const getPosts = require('./components/getPost')
@@ -34,13 +35,20 @@ app.post('/login', async (req, res) => {
 
 app.post('/register', async(req, res) => {
     // Compute a bcrypt hash of the password to be stored in the database
-    const hashPass = bcrypt.hashSync(req.body.password);
-    console.log(hashPass);
-    const registered = await register.registerUser({username: req.body.username, password: hashPass});
-    console.log(registered);
-    res.send({
-        verified: 'yes'
-    });
+    const unique = uniqueUsername(req.body.username);
+    if(unique != null){
+        res.send({
+            verified: 'no'
+        })
+    }else {
+        const hashPass = bcrypt.hashSync(req.body.password);
+        console.log(hashPass);
+        const registered = await register.registerUser({username: req.body.username, password: hashPass});
+        console.log(registered);
+        res.send({
+            verified: 'yes'
+        });
+    }
 });
 
 app.post('/post', async(req, res) => {
