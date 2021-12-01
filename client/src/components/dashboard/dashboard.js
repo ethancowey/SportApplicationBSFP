e8bffa69ffd493ce88179a0335ae880a4d8625f0
Ethan <e.cowey1@newcastle.ac.uk>
2021-12-01 21:27:27 +0000

    Fix to allow tests to run

--- client/src/components/dashboard/dashboard.js
+++ client/src/components/dashboard/dashboard.js
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
-import {useNavigate} from "react-router";
 import '../../App.css';
 import {Link} from "react-router-dom";
 
