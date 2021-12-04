c65277a4fdad28ca612c8607bc0ffa6b0c912c81
Ethan <e.cowey1@newcastle.ac.uk>
2021-12-04 14:13:54 +0000

    Sorts correctly prioritises users favourite sport and then speed

--- server/components/postRanker.js
+++ server/components/postRanker.js
@@ -1,14 +1,22 @@
+const speed = require('./speed')
+
 function ranking(posts, favouriteSport){
     console.log(favouriteSport);
     const sortedPosts = posts.sort(
         function(a, b){
+            if(a.speedmph === undefined){
+                a.speedmph = speed.speedCalc(a.distance, a.time).mph
+            }
+            if(b.speedmph === undefined){
+                b.speedmph = speed.speedCalc(b.distance, b.time).mph
+            }
             if (a.sport === favouriteSport && b.sport === favouriteSport) {
-                return a.time - b.time
+                return b.speedmph - a.speedmph
             }else if(a.sport === favouriteSport){
                 return -1
             } else if(b.sport === favouriteSport) {
                 return 1
-            } else return a.time - b.time
+            } else return b.speedmph - a.speedmph
         }
     )
     return sortedPosts
