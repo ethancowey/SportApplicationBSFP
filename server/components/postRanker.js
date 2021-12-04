c2d3c9779cf73861d3b2cf5c2c36ec2834e5c3a9
Ethan <e.cowey1@newcastle.ac.uk>
2021-12-04 13:09:17 +0000

    Algorithm works correctly however database requires clensing to add speed to all entries

--- server/components/postRanker.js
+++ server/components/postRanker.js
@@ -2,7 +2,13 @@ function ranking(posts, favouriteSport){
     console.log(favouriteSport);
     const sortedPosts = posts.sort(
         function(a, b){
-            return a.time - b.time
+            if (a.sport === favouriteSport && b.sport === favouriteSport) {
+                return a.time - b.time
+            }else if(a.sport === favouriteSport){
+                return -1
+            } else if(b.sport === favouriteSport) {
+                return 1
+            } else return a.time - b.time
         }
     )
     return sortedPosts
